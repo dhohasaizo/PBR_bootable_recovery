b103d79510cfceca60a25ca7cfc8cd155aa8d64a
dhohasaizo <doha.saizo@gmail.com>
2019-11-09 13:09:37 +0700

    fix

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -508,12 +508,12 @@ TWPartitionManager::Output_Partition (TWPartition * Part)
   unsigned long long mb = 1048576;
 
   printf ("%s | %s | Size: %.1lfMB", Part->Mount_Point.c_str (),
-	  Part->Actual_Block_Device.c_str (), (int) (double)Part->Size / mb);
+	  Part->Actual_Block_Device.c_str (), (double)Part->Size / mb);
   if (Part->Can_Be_Mounted)
     {
       printf (" Used: %.1lfMB Free: %.1lfMB Backup Size: %.1lfMB",
-	      (double) Part->Used / mb, (double) Part->Free / mb,
-	      (double) Part->Backup_Size / mb);
+	      (double)Part->Used / mb, (double)Part->Free / mb,
+	      (double)Part->Backup_Size / mb);
     }
   printf ("\n   Flags: ");
   if (Part->Can_Be_Mounted)
